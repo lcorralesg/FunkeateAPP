8df7c8143f16a18ed048320f411287245c9ad48d
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-05-30 13:02:57 -0500

    first commit

---
+++ app/src/main/java/com/corrales/luis/funkeateapp/ApiClient.kt
@@ -0,0 +1,20 @@
+package com.corrales.luis.funkeateapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient {
+
+    private lateinit var apiService: ApiService
+
+    fun getApiService(): ApiService {
+        if (!::apiService.isInitialized) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("bttp://192.168.0.114:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            apiService = retrofit.create(ApiService::class.java)
+        }
+        return apiService
+    }
+}
---
+++ app/src/main/java/com/corrales/luis/funkeateapp/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.corrales.luis.funkeateapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.management.ManagementException
+import com.auth0.android.management.UsersAPIClient
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
+import retrofit2.Call
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var account: Auth0
+    private lateinit var btnLogin: Button
+    private lateinit var btnLogout: Button
+    private lateinit var txtEmail: TextView
+    private lateinit var txtName: TextView
+    private lateinit var txtPictureURL: TextView
+    private lateinit var txtNickname: TextView
+    private lateinit var txtCountry: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set up the account object with the Auth0 application details
+        account = Auth0(
+            "nvyWbZeudGH07w6Ybo5iDKw197sZQ0ns",
+            "dev-gb61hjary7ea583j.us.auth0.com"
+        )
+        setContentView(R.layout.activity_main)
+        // Al presionar el botón loginWithBrowser se ejecuta la función loginWithBrowser
+        btnLogin = findViewById(R.id.btnLogin)
+        btnLogout = findViewById(R.id.btnLogout)
+        txtEmail = findViewById(R.id.txtEmail)
+        txtName = findViewById(R.id.txtName)
+        txtPictureURL = findViewById(R.id.txtPictureURL)
+        txtNickname = findViewById(R.id.txtNickname)
+        txtCountry = findViewById(R.id.txtCountry)
+        btnLogin.setOnClickListener {
+            loginWithBrowser()
+        }
+        btnLogout.setOnClickListener {
+            logout()
+        }
+
+    }
+    private fun loginWithBrowser() {
+        // Setup the WebAuthProvider, using the custom scheme and scope.
+
+        WebAuthProvider.login(account)
+            .withScheme("demo")
+            // modify the scopes to enable read and write of user_metadata
+            .withScope("openid profile email read:current_user update:current_user_metadata")
+            // specify the audience for the Auth0 Management API
+            .withAudience("https://dev-gb61hjary7ea583j.us.auth0.com/api/v2/")
+            // Launch the authentication passing the callback where the results will be received
+            .start(this, object : Callback<Credentials, AuthenticationException> {
+                // Called when there is an authentication failure
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                }
+
+                // Called when authentication completed successfully
+                override fun onSuccess(credentials: Credentials) {
+                    // Get the access token from the credentials object.
+                    // This can be used to call APIs
+                    val accessToken = credentials.accessToken
+                    showUserProfile(accessToken)
+                    val client = AuthenticationAPIClient(account)
+                    client.userInfo(accessToken)
+                        .start(object : Callback<UserProfile, AuthenticationException> {
+                            override fun onFailure(exception: AuthenticationException) {
+                                // Manejar el error
+                            }
+
+                            override fun onSuccess(profile: UserProfile) {
+                                val userId = profile.getId()
+                                if (userId != null) {
+                                    getUserMetadata(userId, accessToken)
+                                }
+                            }
+                        })
+
+                }
+            })
+
+    }
+    private fun logout() {
+        WebAuthProvider.logout(account)
+            .withScheme("demo")
+            .start(this, object: Callback<Void?, AuthenticationException> {
+                override fun onSuccess(payload: Void?) {
+                    // The user has been logged out!
+                    txtEmail.text = ""
+                    txtName.text = ""
+                    txtPictureURL.text = ""
+                    txtNickname.text = ""
+                    txtCountry.text = ""
+                }
+
+                override fun onFailure(error: AuthenticationException) {
+                    // Something went wrong!
+
+                }
+            })
+    }
+    private fun showUserProfile(accessToken: String) {
+        var client = AuthenticationAPIClient(account)
+
+        // With the access token, call `userInfo` and get the profile from Auth0.
+        client.userInfo(accessToken)
+            .start(object : Callback<UserProfile, AuthenticationException> {
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                }
+
+                override fun onSuccess(profile: UserProfile) {
+                    // We have the user's profile!
+                    val email = profile.email
+                    val name = profile.name
+                    val pictureURL = profile.pictureURL
+                    val nickname = profile.nickname
+
+                    txtEmail.text = email
+                    txtName.text = name
+                    txtPictureURL.text = pictureURL
+                    txtNickname.text = nickname
+
+                }
+            })
+    }
+    fun getUserMetadata(userId: String, accessToken: String) {
+        // Get the user ID and call the full getUser Management API endpoint, to retrieve the full profile information
+        // Create the user API client using the account details and the access token from Credentials
+        val usersClient = UsersAPIClient(account, accessToken)
+
+        // Get the full user profile
+        usersClient
+            .getProfile(userId)
+            .start(object: Callback<UserProfile, ManagementException> {
+                override fun onFailure(exception: ManagementException) {
+                    // Something went wrong!
+                }
+
+                override fun onSuccess(profile: UserProfile) {
+                    // Retrieve the "country" field, if one appears in the metadata
+                    val country = profile.getUserMetadata()["country"] as String?
+                    txtCountry.text = country
+                }
+            })
+    }
+}
